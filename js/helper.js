19544581627d4bc592019cde9e1a19c539cade5f
bingram78 <blakeingram@gmail.com>
2016-04-26 11:42:21 +0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -49,7 +49,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<div class="education-entry"><h3 style= "padding-left:0px; color: #999">Online Classes</h3></div>';
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
